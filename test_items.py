4a73e259c016eb46b8370b86caa7013365dc2bfd
lotov <lotov@t8.ru>
2025-07-24 18:06:54 +0300

    Homework  module 3 done

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_guest_should_see_login_link(browser):
+def test_check_basket_button_exists(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button_add_basket = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
